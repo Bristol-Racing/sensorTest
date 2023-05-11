770e1c2e763539bea682507a546bfbee721cf4de
TBExtent <jedpreist@gmail.com>
2023-05-11 17:35:52 +0100

    ehh

--- sensorTest.ino
+++ sensorTest.ino
@@ -1,9 +1,32 @@
 
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <SD.h>
+
+#include "sensors/check.hpp"
 #include "sensors/testcounter.hpp"
 #include "sensors/throttle.hpp"
 #include "sensors/sensorManager.hpp"
 
 #define THROTTLE_PIN A0
+
+#define RFM95_CS  4
+#define RFM95_INT 3
+#define RFM95_RST 2
+
+#define RF95_FREQ 915.0
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+#define SD_CS 5
+
+const char filename[] = "log.txt";
+
+// File object to represent file
+// File txtFile;
+
+// string to buffer output
+// String buffer;
 
 const double arm_length = 142.5 / 1000.0;  //  meters
 
@@ -22,6 +45,9 @@ const int readings = 10;
 Sensor::SensorManager manager(sensorCount, time_per_reading * readings);
 
 void setup() {
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+
     Serial.begin(57600);
 
     counter1.setReadRate(1000);
@@ -38,25 +64,59 @@ void setup() {
     manager.addSensor(monitor);
 
     manager.setReadCallback(&readCallback);
+
+    // buffer.reserve(1024);
+
+    // bool sdStatus = SD.begin(SD_CS);
+    // CHECK(sdStatus == true, "SD initialisation failed.");
+
+    // txtFile = SD.open(filename, FILE_WRITE);
+    // CHECK(txtFile, "Error opening log file.");
+
+    // txtFile.println("AAAAA");
+
+    // Serial.println("AAAAA");
+
+    delay(100);
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
+
+    bool initStatus = rf95.init();
+    CHECK(initStatus == true, "LoRa initialisation failed.");
+
+    bool freqStatus = rf95.setFrequency(RF95_FREQ);
+    CHECK(freqStatus == true, "LoRa frequency set failed.");
+
+    rf95.setTxPower(23, false);
 }
 
 void loop() {
+    Serial.println("B");
     manager.spin(5000);
     Serial.println(manager.getLastRead(&counter1));
 }
 
 void readCallback(double * results) {
+    rf95.send((uint8_t*)results, sensorCount * sizeof(double));
+
     for (int i = 0; i < sensorCount; i++) {
         if (i > 0) {
             Serial.print(",");
+            // txtFile.print(",");
         }
         Serial.print(results[i]);
+        // txtFile.print(results[i]);
     }
     Serial.println();
+    // txtFile.println();
+
+    rf95.waitPacketSent();
 }
 
 void throttleCallback(double voltage) {
     // if (isnan(voltage)) {
-        Serial.println(voltage);
+        // Serial.println(voltage);
     // }
 }
